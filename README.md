7f7f0e0d5f33f2e084929a575595eecb039b5763
Mugzx <158086960+Mugzx@users.noreply.github.com>
2025-11-17 23:49:55 +0800

    init: initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# 💤 LazyVim
+
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+## 配置项
+
+```sh
+lua
+├── config
+│   ├── autocmds.lua # 行内提示优化
+│   ├── keymaps.lua # 键位映射
+│   ├── lazy.lua # 插件管理
+│   └── options.lua # 全局设置
+└── components
+    ├── codeium.lua # Codeium AI
+    ├── colorscheme.lua # Catppuccin 主题配色（亚克力透明背景）
+    └── tiDiagonstic.lua # 行内提示优化
+```
+
+## 参考
+
+感谢[H0ron1ee](https://github.com/horonlee)提供的开发配置指~~北~~！
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = function()
+        require("catppuccin").load()
+      end,
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      -- 设置为 false 可重新开启背景颜色显示
+      transparent_background = true,
+      color_overrides = {
+        mocha = {
+          base = "#060b16",
+          mantle = "#090f1d",
+          crust = "#0b1625",
+        },
+      },
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
+}
